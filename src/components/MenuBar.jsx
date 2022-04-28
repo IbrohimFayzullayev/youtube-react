8abf81cb47d2ec10e3aa371b97a6a8e6a20f425d
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-28 11:37:21 +0500

    youtube project with react

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+class MenuBar extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="menu__bar">
+        <div className="hamburger">
+          <div className="hamburger__line"></div>
+          <div className="hamburger__line"></div>
+          <div className="hamburger__line"></div>
+        </div>
+        <div className="menu__icons">
+          <div className="menu__item">
+            <i className="home icon"></i>
+            <p className="menu__item--info">Home</p>
+          </div>
+          <div className="menu__item">
+            <i className="compass outline icon"></i>
+            <p className="menu__item--info">Navigator</p>
+          </div>
+          <div className="menu__item">
+            <i className="play circle outline icon"></i>
+            <p className="menu__item--info">Shorts</p>
+          </div>
+          <div className="menu__item">
+            <i className="folder open outline icon"></i>
+            <p className="menu__item--info">Folders</p>
+          </div>
+          <div className="menu__item">
+            <i className="history icon"></i>
+            <p className="menu__item--info">Library</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MenuBar;
