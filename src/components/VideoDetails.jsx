a70e7af90f1cfbf31646cf2aa2fc82e99eb9e7ba
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-28 11:37:35 +0500

    youtube project with react

---
+++ src/components/VideoDetails.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "../Styles.css";
+// import axios from "axios";
+class VideoDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { arr: "", obj: {}, videoId: "" };
+  }
+
+  getVievsInfo = async (video) => {
+    let url = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${video}&key=AIzaSyDXZdLg3XezWfbwftMKP7r2mJyzOf2AO1Q`;
+    const info = await fetch(url);
+    const jsonOl = await info.json();
+    return jsonOl.items[0].statistics;
+  };
+
+  getChannelInfo = async (id) => {
+    const data = await fetch(
+      `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${id}&fields=items(id%2Csnippet%2Fthumbnails)&key=AIzaSyDXZdLg3XezWfbwftMKP7r2mJyzOf2AO1Q`
+    );
+    const getJson = await data.json();
+    return getJson.items[0].snippet.thumbnails.default.url;
+  };
+  async componentDidMount() {
+    let a = await this.getChannelInfo(this.props.info.snippet.channelId);
+    let b = await this.getVievsInfo(this.props.info.id.videoId);
+    this.setState({ arr: a, obj: b });
+  }
+  hisobla = (num) => {
+    let str = String(num);
+    if (str.length < 4) {
+      return num;
+    } else {
+      return (num / 1000).toFixed(2) + " Thousand";
+    }
+  };
+  getIdClick = () => {
+    // this.setState({ videoId: this.props.info.id.videoId });
+    // this.props.vidId = this.props.info.id.videoId;
+    this.props.vidId(this.props.info.id.videoId);
+    // console.log(this.props.vidId);
+    // console.log(this.props.info.id.videoId);
+  };
+  render() {
+    return (
+      <div className="video_box" key={this.props.info.id.videoId}>
+        <img
+          className="video_box--img"
+          src={this.props.info.snippet.thumbnails.medium.url}
+          alt={"images"}
+        />
+        <div className="video__info">
+          <p onClick={this.getIdClick} className="heading_secondary">
+            {this.props.info.snippet.title} {this.state.videoId}
+          </p>
+          <p className="video__report">
+            {this.hisobla(this.state.obj.viewCount)} views
+          </p>
+          <div className="channel__name">
+            <img className="channel__img" src={this.state.arr} alt="img" />
+            <h3 className="channel__heading">
+              {this.props.info.snippet.channelTitle}
+            </h3>
+          </div>
+          <p className="video__description">
+            {this.props.info.snippet.description}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VideoDetails;
