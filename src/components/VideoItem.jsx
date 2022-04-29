0818705fafc3f662cbc1f1ae648a8cc716b838da
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-29 10:27:02 +0500

    project

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -6,16 +6,26 @@ class VideoItem extends React.Component {
   }
   render() {
     return (
-      <div>
-        <iframe
-          width="400"
-          height="400"
-          src={"https://www.youtube.com/embed/" + this.props.dataId}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+      <div className="iframe__box">
+        <div className="fixed__box">
+          <iframe
+            width="400"
+            height="400"
+            src={"https://www.youtube.com/embed/" + this.props.dataId}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <div className="video__desc">
+            <h3 className="video__title">{this.props.dataRepo.title}</h3>
+            <div className="video__reports">
+              <p className="video__date">
+                {this.props.dataRepo.repo.viewCount} views
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
