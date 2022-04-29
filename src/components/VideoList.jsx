a061dd09b4c6ca48aeab0ffd64185d8ad430f8f8
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-29 10:27:08 +0500

    project

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -5,16 +5,29 @@ import "../Styles.css";
 class VideoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { videoData: [] };
+    this.state = { videoData: [], classNomi: "" };
   }
 
+  getClassName = (name) => {
+    this.setState({ classNomi: name });
+  };
   renderList = () => {
     return this.props.data.data?.items.map((val) => {
-      return <VideoDetails vidId={this.props.idVideo} info={val} />;
+      return (
+        <VideoDetails
+          vidId={this.props.idVideo}
+          info={val}
+          klass={this.getClassName}
+        />
+      );
     });
   };
   render() {
-    return <div className="videos">{this.renderList()}</div>;
+    return (
+      <div className={"videos " + this.state.classNomi}>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
